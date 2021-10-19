92e894d0c24c103da356e8a63961d8875fa6ca8a
José Erivam <erivamdev@gmail.com>
2021-10-19 14:22:46 -0300

    adding requisite two incomplete

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div data-testid="page-login">Login</div>;
+    return (
+      <div data-testid="page-login">
+        <form action="">
+          <label htmlFor="name-input">
+            <input type="text" data-testid="login-name-input" />
+          </label>
+          <button type="submit" name="" data-testid="login-submit-button">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 

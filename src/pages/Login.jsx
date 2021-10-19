1a215c3343f8f4a101f0b54949e70816781135a1
José Erivam <erivamdev@gmail.com>
2021-10-19 16:52:55 -0300

    adding requisite two

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,66 @@
 import React from 'react';
+import { Redirect } from 'react-router';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userName: '',
+      isSaveButtonDisabled: true,
+      loading: false,
+      redirect: false,
+    };
+    this.makeChange = this.makeChange.bind(this);
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
+
+  async handleOnClick(userName) {
+    this.setState({ loading: true });
+    await createUser({ name: userName });
+    this.setState({ redirect: true });
+  }
+
+  makeChange({ target }) {
+    this.setState({ userName: target.value }, this.checkButton);
+  }
+
+  checkButton() {
+    const { userName } = this.state;
+    const minLength = 3;
+    if (userName.length >= minLength) {
+      this.setState({ isSaveButtonDisabled: false });
+    } else {
+      this.setState({ isSaveButtonDisabled: true });
+    }
+  }
+
   render() {
+    const { userName, isSaveButtonDisabled, loading, redirect } = this.state;
+    if (loading) {
+      return redirect ? <Redirect to="/search" /> : <Loading />;
+    }
     return (
       <div data-testid="page-login">
-        <form action="">
-          <label htmlFor="name-input">
-            <input type="text" data-testid="login-name-input" />
+        <form>
+          <label htmlFor="login-name-input">
+            <input
+              type="text"
+              data-testid="login-name-input"
+              value={ userName }
+              onChange={ this.makeChange }
+            />
           </label>
-          <button type="submit" name="" data-testid="login-submit-button">Entrar</button>
+          <button
+            type="submit"
+            data-testid="login-submit-button"
+            disabled={ isSaveButtonDisabled }
+            onClick={ () => this.handleOnClick(userName) }
+          >
+            Entrar
+
+          </button>
         </form>
       </div>
     );

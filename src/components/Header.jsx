c648257043b648c2011bebf8321dd7f0402b8b20
José Erivam <erivamdev@gmail.com>
2021-10-19 21:26:49 -0300

    adding requisite four

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Loading from '../pages/Loading';
 import { getUser } from '../services/userAPI';
 
@@ -20,6 +21,15 @@ class Header extends React.Component {
     return (
       <header data-testid="header-component">
         {loading ? <Loading /> : <p data-testid="header-user-name">{name}</p>}
+        <Link to="/search" data-testid="link-to-search">
+          Pesquisa
+        </Link>
+        <Link to="/favorites" data-testid="link-to-favorites">
+          Favoritos
+        </Link>
+        <Link to="/profile" data-testid="link-to-profile">
+          Perfil
+        </Link>
       </header>
     );
   }

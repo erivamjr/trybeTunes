349fcb279872422e11d93adb9f2d29b3d8705eb7
José Erivam <erivamdev@gmail.com>
2021-10-21 18:07:23 -0300

    adding requisite six

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,14 +1,61 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from './MusicCard';
 
 class Album extends React.Component {
+  constructor() {
+    super();
+    this.state = ({
+      musics: [],
+    });
+  }
+
+  componentDidMount() { // Essa função é executada no momento que o componente é montado. Por isso funciona como um evento uma vez que foi criado um link em todos os albuns, então uma vez que vc clica nele 'gera' um novo componente
+    const { match: { params: { id } } } = this.props;
+    getMusics(id).then((response) => {
+      this.setState({
+        musics: response,
+      });
+    });
+  }
+
   render() {
     return (
       <div data-testid="page-album">
         <Header />
+        <div data-testid="page-album">
+          {musics.length
+          && (
+            <div>
+              <img
+                src={ infoAlbuns.artworkUrl100 }
+                alt={ infoAlbuns.collectionName }
+              />
+              <h4 data-testid="artist-name">{ infoAlbuns.artistName }</h4>
+              <h4 data-testid="album-name">{ infoAlbuns.collectionName }</h4>
+            </div>
+          )}
+          { musics
+            .filter((music) => music.trackId)
+            .map((music, index) => (
+              <MusicCard
+                key={ index }
+                music={ music }
+              />
+            ))}
+        </div>
       </div>
     );
   }
 }
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default Album;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 import Header from '../components/Header';
+import Loading from './Loading';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
       searchAlbum: '',
+      actualAlbum: '',
+      nameInput: '',
+      loading: false,
       isSaveButtonDisabled: true,
+      albums: [],
     };
     this.makeChange = this.makeChange.bind(this);
+    this.searchAlbumsMusic = this.searchAlbumsMusic.bind(this);
   }
 
   makeChange({ target }) {
@@ -25,30 +33,75 @@ class Search extends React.Component {
     }
   }
 
+  searchAlbumsMusic(e) {
+    e.preventDefault();
+    const { searchAlbum } = this.state;
+    this.setState(
+      { searchAlbum: '', loading: true }, // esperando loading para true para depois execulcar a callback
+      () => searchAlbumsAPI(searchAlbum).then((promise) => {
+        console.log(promise);
+        this.setState({ loading: false,
+          albums: promise,
+          nameInput: searchAlbum });
+      }),
+    );
+  }
+
   render() {
-    const { searchAlbum, isSaveButtonDisabled } = this.state;
+    const { searchAlbum,
+      isSaveButtonDisabled,
+      loading,
+      albums,
+      nameInput } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <form action="">
-          <label htmlFor="search-artist-input">
-            <input
-              type="text"
-              name="searchAlbum"
-              data-testid="search-artist-input"
-              value={ searchAlbum }
-              onChange={ this.makeChange }
-            />
-          </label>
-          <button
-            type="submit"
-            data-testid="search-artist-button"
-            disabled={ isSaveButtonDisabled }
-          >
-            Pesquisar
+        {loading ? <Loading /> : (
+          <form action="">
+            <label htmlFor="search-artist-input">
+              <input
+                type="text"
+                name="searchAlbum"
+                data-testid="search-artist-input"
+                value={ searchAlbum }
+                onChange={ this.makeChange }
+              />
+            </label>
+            <button
+              type="submit"
+              data-testid="search-artist-button"
+              disabled={ isSaveButtonDisabled }
+              onClick={ this.searchAlbumsMusic }
+            >
+              Pesquisar
 
-          </button>
-        </form>
+            </button>
+          </form>
+        )}
+        <p>
+          {`Resultado de álbuns de: ${nameInput}`}
+        </p>
+        {albums.length
+          ? (
+            <div>
+              <ul>
+                {albums
+                  .map(({ collectionName, collectionId, artworkUrl100 }) => (
+                    <li key={ collectionId }>
+                      <img src={ artworkUrl100 } alt={ collectionName } />
+                      <Link
+                        data-testid={ `link-to-album-${collectionId}` }
+                        to={ `/album/${collectionId}` }
+                      >
+                        {collectionName}
+                      </Link>
+                    </li>
+                  ))}
+              </ul>
+            </div>
+          )
+          : <p>Nenhum álbum foi encontrado</p>}
+
       </div>
     );
   }
